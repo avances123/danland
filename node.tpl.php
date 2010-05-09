451c3b559667becfa03e49e241385f20a3a29f31
Danang Probo Sayekti <dan@danpros.com>
2010-05-09 00:00:19 +0000

    Resolving the XHTML error in taxonomy terms.

--- node.tpl.php
+++ node.tpl.php
@@ -7,7 +7,7 @@
     }?>
     <?php if ($page == 0) { ?><h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2><?php }; ?>
     <?php if ($submitted): ?><span class="submitted"><?php print $submitted; ?></span><?php endif; ?>
-    <?php if ($taxonomy): ?><span class="taxonomy"><?php print $terms; ?></span><?php endif;?>
+    <?php if ($taxonomy): ?><div class="taxonomy"><?php print $terms; ?></div><?php endif;?>
     <div class="content"><?php print $content; ?></div>
     <?php if ($links): ?>
       <div class="links"><?php print $links; ?></div>
