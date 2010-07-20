fd0be258735d1702ea7271ff8efc08edf8376427
Danang Probo Sayekti <dan@danpros.com>
2010-07-20 01:08:31 +0000

    Updating the notice links.

--- page-front.tpl.php
+++ page-front.tpl.php
@@ -193,6 +193,6 @@ jQuery(document).ready(function($) {
 </div>
 </div>
 <div style="clear:both"></div>
-<div id="notice"><p>Theme by <a href="http://www.danetsoft.com">Danetsoft</a> and <a href="http://www.danpros.com">Danang Probo Sayekti</a> inspired by <a href="http://www.maksimer.no">Maksimer AS</a></p></div><?php print $closure; ?>
+<div id="notice"><p>Theme by <a href="http://www.danetsoft.com">Danetsoft</a> and <a href="http://www.danpros.com">Danang Probo Sayekti</a> inspired by <a href="http://www.maksimer.no">Maksimer</a></p></div><?php print $closure; ?>
 </body>
 </html>
