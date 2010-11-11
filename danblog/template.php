1f83e9e89ee541662dcf48c4fc88d28fd9587800
Danang Probo Sayekti <dan@danpros.com>
2010-11-11 14:08:01 +0000

    Adding template.php to Danblog for D7 version.

---
+++ danblog/template.php
@@ -0,0 +1,7 @@
+<?php
+// $Id$
+
+function danblog_preprocess_html(&$variables) {
+  drupal_add_css(path_to_theme() . '/style.ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+
